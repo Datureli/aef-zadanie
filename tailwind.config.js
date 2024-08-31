15d313744a63d45592f59d6e4ae3a395b7acb10c
Datureli <soleris1@gmail.com>
2024-08-31 06:24:39 +0100

    fix tailwind configuration and add images folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  content: ["./**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+
